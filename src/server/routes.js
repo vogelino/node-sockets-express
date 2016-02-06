3953a1c0dcbe0163aee42707e08fbf2aa746a58a
Lucas Vogel <contact@vogelino.ch>
2016-02-06 20:09:43 +0100

    Server with different response based on the request method

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,13 +1,17 @@
 module.exports = {
-	root: function(response) {
+	root: function(request, response) {
 		response.writeHead(200, { "Content-Type": "text/html" });
 		response.end('Hello <strong>World</strong>!');
 	},
-	goodbye: function(response) {
+	getGoodbye: function(request, response) {
 		response.writeHead(200, { "Content-Type": "text/html" });
-		response.end('Goodbye <strong>World</strong>!');
+		response.end('Goodbye. <strong>Give me a hug</strong>!');
 	},
-	notFound: function(response) {
+	postGoodbye: function(request, response) {
+		response.writeHead(200, { "Content-Type": "text/html" });
+		response.end('Goodbye. <strong>Here, a kiss for you</strong>!');
+	},
+	notFound: function(request, response) {
 		response.writeHead(404, { "Content-Type": "text/html" });
 		response.end('Page <strong>not found</strong>! Sorry about that…');
 	}
--- src/server/server.js
+++ src/server/server.js
@@ -4,13 +4,17 @@ var routes = require('./routes');
 var server = http.createServer(function(request, response) {
 	switch(request.url) {
 		case '/':
-			routes.root(response);
+			routes.root.apply(routes.root, arguments);
 			break;
 		case '/goodbye':
-			routes.goodbye(response);
+			if (request.method === 'POST') {
+				routes.postGoodbye.apply(routes.postGoodbye, arguments);
+			} else {
+				routes.getGoodbye.apply(routes.goodbye, arguments);
+			}
 			break;
 		default:
-			routes.notFound(response);
+			routes.notFound.apply(routes.notFound, arguments);
 			break;
 	}
 });
