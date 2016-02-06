91ba49144e097e7219717fa50d70247b8a563ac6
Lucas Vogel <contact@vogelino.ch>
2016-02-06 19:51:52 +0100

    Server with Hello World in html

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
-	response.writeHead(200, { "Content-Type": "text/plain" });
-	response.end('Hello World!');
+	response.writeHead(200, { "Content-Type": "text/html" });
+	response.end('Hello <strong>World</strong>!');
 });
 
 server.listen(3000);
